ef5cfe18ce6e07bae469451fbe31489dffdf686e
najimRizky <najimajim1@gmail.com>
2023-09-14 08:40:01 +0700

    feat(leaderboard): add user leaderboard

--- src/components/pages/LeaderboardPage/index.tsx
+++ src/components/pages/LeaderboardPage/index.tsx
@@ -34,7 +34,7 @@ const LeaderboardPage = () => {
       ) : (
         <table className="w-full mt-6">
           <thead>
-            <tr className="border-b border-black">
+            <tr className="border-b border-slate-400">
               <th></th>
               <th className="text-left pb-4">Username</th>
               <th className="text-left pb-4">Wins</th>
@@ -45,7 +45,12 @@ const LeaderboardPage = () => {
           </thead>
           <tbody>
             {leaderboardData?.leaderboard?.map((user: any, index: number) => (
-              <tr className="border-b border-black" key={index}>
+              <tr
+                className={`border-b border-slate-400 
+                ${session && index === 3 ? 'bg-red-200 font-bold' : ''}
+                `}
+                key={index}
+              >
                 <td className="p-2 text-right text-xl font-bold">{index + 1}</td>
                 <td className="p-2 text-sm">@{user.username}</td>
                 <td className="p-2">{user.stats.wins}</td>
@@ -54,20 +59,20 @@ const LeaderboardPage = () => {
                 <td className="p-2">{user.stats.percentage}%</td>
               </tr>
             ))}
-            {(session && leaderboardData?.user?.position > 10 ) && (
+            {(session && leaderboardData?.user?.position > 10) && (
               <>
                 <tr>
-                  <td className="h-8">
-                    <div className="border-r-4 h-full border-dotted border-black mr-4 mt-4" />
+                  <td className="h-10 py-2">
+                    <div className="border-r-4 h-full border-dotted border-slate-500 mr-4" />
                   </td>
                 </tr>
-                <tr className="border-b border-black text-green-700">
-                  <td className="px-2 pb-2 pt-4 text-right text-xl font-bold">{leaderboardData?.user?.position}</td>
-                  <td className="px-2 pb-2 pt-4 text-sm">@{leaderboardData?.user?.username}</td>
-                  <td className="px-2 pb-2 pt-4">{leaderboardData?.user?.stats.wins}</td>
-                  <td className="px-2 pb-2 pt-4">{leaderboardData?.user?.stats.losses}</td>
-                  <td className="px-2 pb-2 pt-4">{leaderboardData?.user?.stats.total}</td>
-                  <td className="px-2 pb-2 pt-4">{leaderboardData?.user?.stats.percentage}%</td>
+                <tr className="border-b border-slate-400 bg-red-200 font-bold">
+                  <td className="p-2 text-right text-xl font-bold">{leaderboardData?.user?.position}</td>
+                  <td className="p-2 text-sm">@{leaderboardData?.user?.username}</td>
+                  <td className="p-2">{leaderboardData?.user?.stats.wins}</td>
+                  <td className="p-2">{leaderboardData?.user?.stats.losses}</td>
+                  <td className="p-2">{leaderboardData?.user?.stats.total}</td>
+                  <td className="p-2">{leaderboardData?.user?.stats.percentage}%</td>
                 </tr>
               </>
             )}
