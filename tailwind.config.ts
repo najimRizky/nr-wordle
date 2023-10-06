eafc10a094da2f0e571ec28f675f58181c215ece
najimRizky <najimajim1@gmail.com>
2023-10-06 18:42:27 +0700

    fix(font): bug default font not working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      "body": ['"Nunito"', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
